99f98061ef01f98e1eb05df7ee00a443913dde3f
Danylo687 <danylodobysh@gmail.com>
2026-09-05 22:30:30 +0300

    Add adaptation for diffrent screen sizes

--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -2,16 +2,40 @@ import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class HeaderComponent {
   isScrolled = false;
+  isMobileMenuOpen = false;
+  isKnivesExpanded = false;
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    // Хедер змінюватиме колір після того, як користувач проскролить 50 пікселів вниз
     this.isScrolled = window.scrollY > 50;
+  }
+
+  /* Повернення на самий верх сторінки */
+  scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  /* Керування мобільним меню */
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    // Блокуємо скрол сторінки, коли меню відкрите
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  /* Керування списком "Ножі" в мобільному меню */
+  toggleKnives(event: Event) {
+    event.preventDefault();
+    this.isKnivesExpanded = !this.isKnivesExpanded;
   }
 }
